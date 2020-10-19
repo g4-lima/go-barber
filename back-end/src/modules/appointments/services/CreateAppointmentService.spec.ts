3339d1509f23fdcf55befd7a719b8a7fd3fe2515
Gabriel Lima <gabastos.lima@gmail.com>
2020-10-19 17:37:42 -0300

    covarege tests

--- back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ back-end/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -16,7 +18,24 @@ describe('CreateAppointment', () => {
         expect(appointment).toHaveProperty('id');
     });
 
-    // it('should not be able to create a new appointment on the same date', () => {
-    //     expect(1 + 2).toBe(3);
-    // });
+    it('should not be able to create a new appointment on the same date', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '1231312313',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '1231312313',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
