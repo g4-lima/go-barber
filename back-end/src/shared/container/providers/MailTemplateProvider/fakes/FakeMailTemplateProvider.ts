6e851b7085a669b2d52f09bc9ab877c749f0f57c
Gabriel Lima <gabastos.lima@gmail.com>
2020-11-23 23:42:35 -0300

    email password reset

--- back-end/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ back-end/src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
-import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 import IMailTemplateProvier from '../models/IMailTemplateProvider';
 
 class FakeMailTemplateProvider implements IMailTemplateProvier {
-    public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
-        return template;
+    public async parse(): Promise<string> {
+        return 'Mail content';
     }
 }
 
