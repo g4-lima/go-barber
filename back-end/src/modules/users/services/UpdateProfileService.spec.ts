5d3a39d10e696717239e541b01dfb83fbf985ef2
Gabriel Lima <gabastos.lima@gmail.com>
2020-11-25 22:56:18 -0300

    tests update password

--- back-end/src/modules/users/services/UpdateProfileService.spec.ts
+++ back-end/src/modules/users/services/UpdateProfileService.spec.ts
@@ -1,5 +1,4 @@
-// import AppError from '@shared/errors/AppError';
-
+import AppError from '@shared/errors/AppError';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import UpdateProfileService from './UpdateProfileService';
@@ -34,5 +33,63 @@ describe('UpdateProfile', () => {
 
         expect(updatedUser.name).toBe('John Tre');
         expect(updatedUser.email).toBe('johntre@example.com');
+    });
+
+    it('should not be able to change to an already used email', async () => {
+        await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@exemple.com',
+            password: '123456',
+        });
+
+        const user = await fakeUsersRepository.create({
+            name: 'Test',
+            email: 'test@exemple.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfile.execute({
+                user_id: user.id,
+                name: 'Test',
+                email: 'johndoe@exemple.com',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should be able to update the password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@exemple.com',
+            password: '123456',
+        });
+
+        const updatedUser = await updateProfile.execute({
+            user_id: user.id,
+            name: 'John Tre',
+            email: 'johntre@example.com',
+            old_password: '123456',
+            password: '123123',
+        });
+
+        expect(updatedUser.password).toBe('123123');
+    });
+
+    it('should not be able to update the password without typing the old one', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@exemple.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfile.execute({
+                user_id: user.id,
+                name: 'John Tre',
+                email: 'johntre@example.com',
+                old_password: 'wrong-old-password',
+                password: '123123',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
--- back-end/src/modules/users/services/UpdateProfileService.ts
+++ back-end/src/modules/users/services/UpdateProfileService.ts
@@ -23,14 +23,48 @@ class UpdateProfile {
         private hashProvider: IHashProvider,
     ) {}
 
-    public async execute({ name, user_id, email }: IRequest): Promise<User> {
+    public async execute({
+        name,
+        user_id,
+        email,
+        password,
+        old_password,
+    }: IRequest): Promise<User> {
         const user = await this.usersRepository.findById(user_id);
 
         if (!user) {
-            throw new AppError('User not found!');
+            throw new AppError('User not found.');
         }
 
-        return user;
+        const userWithUpdatedEmail = await this.usersRepository.findByEmail(
+            email,
+        );
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
+            throw new AppError('E-mail already in use.');
+        }
+
+        user.name = name;
+        user.email = email;
+
+        if (password && !old_password) {
+            throw new AppError('Old password not informed.');
+        }
+
+        if (password && old_password) {
+            const checkOldPassword = await this.hashProvider.compareHash(
+                old_password,
+                user.password,
+            );
+
+            if (!checkOldPassword) {
+                throw new AppError('Old password is incorrect');
+            }
+
+            user.password = await this.hashProvider.generateHash(password);
+        }
+
+        return this.usersRepository.save(user);
     }
 }
 
